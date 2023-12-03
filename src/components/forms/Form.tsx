fa4be7f1821d5db71ad5576892a79c0a227f6d16
sabbir <sabbir@gmail.com>
2023-12-04 00:10:31 +0600

    setup react hook form

---
+++ src/components/forms/Form.tsx
@@ -0,0 +1,37 @@
+import { ReactElement, ReactNode } from "react";
+import {
+  useForm,
+  FormProvider,
+  SubmitHandler,
+  useFormContext,
+} from "react-hook-form";
+
+type FromConfig = {
+  defaultValues?: Record<string, any>;
+};
+
+type FromProps = {
+  children?: ReactElement | ReactNode;
+  submitHandler: SubmitHandler<any>;
+} & FromConfig;
+
+const Form = ({ children, submitHandler, defaultValues }: FromProps) => {
+  const fromConfig: FromConfig = {};
+
+  if (!!defaultValues) fromConfig["defaultValues"] = defaultValues;
+
+  const methods = useForm<FromProps>(fromConfig);
+  const { handleSubmit, reset } = methods;
+  const onSubmit = (data: any) => {
+    submitHandler(data);
+    reset();
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default Form;
