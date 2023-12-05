7ebff8a7877f6956c03d2dd15f9bba9bcd9c7384
sabbir <sabbir@gmail.com>
2023-12-05 19:22:08 +0600

    breadCrumb

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,10 +1,29 @@
 "use client";
 import { Breadcrumb, Layout, Menu, theme } from "antd";
+import UMBreadcrumb from "./UMBreadcrumb";
 
 const { Header, Content, Footer, Sider } = Layout;
 
+const base = "admin";
+
 const Contents = ({ children }: { children: React.ReactNode }) => {
-  return <Content style={{ height: "100vh" }}>{children}</Content>;
+  return (
+    <Content style={{ height: "100vh" }}>
+      <UMBreadcrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
+          {
+            label: `student`,
+            link: `/${base}/student`,
+          },
+        ]}
+      />
+      {children}
+    </Content>
+  );
 };
 
 export default Contents;
